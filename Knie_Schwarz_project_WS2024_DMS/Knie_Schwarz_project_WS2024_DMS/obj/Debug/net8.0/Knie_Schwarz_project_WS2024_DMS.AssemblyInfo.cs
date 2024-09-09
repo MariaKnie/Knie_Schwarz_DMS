c8fdc33bcc2495aa7d71b22e8e0fae72a7b20490
MariaKnie <mariaknie@gmx.at>
2024-09-09 17:04:12 +0200

    Brainstorm file

--- Knie_Schwarz_project_WS2024_DMS/Knie_Schwarz_project_WS2024_DMS/obj/Debug/net8.0/Knie_Schwarz_project_WS2024_DMS.AssemblyInfo.cs
+++ Knie_Schwarz_project_WS2024_DMS/Knie_Schwarz_project_WS2024_DMS/obj/Debug/net8.0/Knie_Schwarz_project_WS2024_DMS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Knie_Schwarz_project_WS2024_DMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3853535eb6cead135bc3048f1ea34a69b26f5f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efef4496caddd695e5ad9eba7f29e0fb53e3bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Knie_Schwarz_project_WS2024_DMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Knie_Schwarz_project_WS2024_DMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
