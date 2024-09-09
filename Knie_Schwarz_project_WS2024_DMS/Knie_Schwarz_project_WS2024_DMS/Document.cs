78e591ccf5b646a72e0f4ada3ab5b9ab3e138d37
Sevi <if22b225@technikum-wien.at>
2024-09-09 17:25:03 +0200

    Added document class

---
+++ Knie_Schwarz_project_WS2024_DMS/Knie_Schwarz_project_WS2024_DMS/Document.cs
@@ -0,0 +1,14 @@
+﻿namespace Knie_Schwarz_project_WS2024_DMS
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public DateTime DateUpload { get; set; }
+        public DateTime DateEdit { get; set; }
+        public string? Author { get; set; }
+
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+    }
+}
+
