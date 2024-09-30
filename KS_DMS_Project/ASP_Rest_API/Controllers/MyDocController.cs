198b7a72f94bc86fe76d7e586dcce002571550b3
MariaKnie <mariaknie@gmx.at>
2024-09-30 12:22:17 +0200

    added our Doc Model

---
+++ KS_DMS_Project/ASP_Rest_API/Controllers/MyDocController.cs
@@ -0,0 +1,97 @@
+﻿using ASP_Rest_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace ASP_Api_Demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyDocController : ControllerBase
+    {
+
+        private static List<MyDoc> documents = new List<MyDoc>
+        {
+           new MyDoc(1, "whale" , DateTime.Now, DateTime.Now, "Ann", "me"),
+           new MyDoc(2, "fish" , DateTime.Now, DateTime.Now, "Bob", "ma"),
+           new MyDoc(3, "deer" , DateTime.Now, DateTime.Now, "Cindy", "mu")
+        };
+
+        /// <summary>
+        /// Gibt eine Liste von MyDocs zurück. Optional können Titel und/oder Author gefiltert werden
+        /// </summary>
+        /// <param name="author">Der Author des MyDocs, nach dem gefiltert werden kann (string, optional)</param>
+        /// <param name="title">Der Title des MyDocs, (string, optional)</param>
+        /// <returns>IEnumearble<MyDoc></MyDoc></returns>
+        [HttpGet]
+        public IEnumerable<MyDoc> Get([FromQuery] string? author, [FromQuery] string? title)
+        {
+            var items = documents.AsEnumerable();
+
+            //Nach Name filterm wenn ein Name übergeben wurde
+            if (!string.IsNullOrWhiteSpace(author))
+            {
+                items = items.Where(t => t.Author.Contains(author));
+            }
+
+            //Nach Name filterm wenn ein Name übergeben wurde
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                items = items.Where(t => t.Title.Contains(title));
+            }
+
+            return items;
+        }
+
+        [HttpPost]
+        public ActionResult<MyDoc> PostTodoItem(MyDoc item)
+        {
+            // Überprüfung, ob der Title-Name leer oder null ist
+            if (string.IsNullOrWhiteSpace(item.Title))
+            {
+                return BadRequest(new { message = "Doc Title cannot be empty." });
+            }
+
+            // Neue ID generieren: Falls die Liste leer ist, starte mit ID 1
+            if (documents.Any())
+            {
+                item.Id = documents.Max(t => t.Id) + 1; // Generiere die nächste ID basierend auf dem Maximalwert
+            } else
+            {
+                item.Id = 1; // Falls die Liste leer ist, starte mit ID 1
+            }
+
+            documents.Add(item);
+
+            // Erfolgreich erstellt zurückgeben
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult PutTodoItem(int id, MyDoc item)
+        {
+            var existingItem = documents.FirstOrDefault(t => t.Id == id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            existingItem.Title = item.Title;
+            existingItem.Author = item.Author;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodoItem(int id)
+        {
+            var item = documents.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            documents.Remove(item);
+            return NoContent();
+        }
+    }
+}
--- KS_DMS_Project/WebUI/wwwroot/app.js
+++ KS_DMS_Project/WebUI/wwwroot/app.js
@@ -1,4 +1,4 @@
-﻿const apiUrl = 'http://localhost:8080/todo';
+﻿const apiUrl = 'http://localhost:8081/todo';
 
 // Function to fetch and display Todo items
 function fetchTodoItems() {
